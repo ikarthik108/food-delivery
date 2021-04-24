9a55f18e3ac21207693458dce4f5b9be57f7dfdc
ikarthik108 <iyerkarthik108@gmail.com>
2021-04-24 17:42:43 +0530

    Updating our menu items to navigate using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Homepage from './Pages/Homepage/Homepage.js'
+import {Switch,Route} from 'react-router-dom';
+
+const DosaVarietyPage=()=> {
+	return (
+		<div>
+			<h1>You are on the Dosa Varieties Page</h1>
+		</div>
+		)
+}
 
 function App() {
   return (
     <div className="">
-      <Homepage/>
+      <Route exact path='/' component={Homepage}/>
+      <Route path='/dosa' component={DosaVarietyPage}/>
     </div>
   );
 }
--- src/Components/Directory/Directory.js
+++ src/Components/Directory/Directory.js
@@ -10,29 +10,34 @@ class Directory extends React.Component {
 			  {
 			    title: 'Dosa varieties',
 			    imageUrl: 'https://www.bigbasket.com/media/uploads/recipe/w-l/1778_6_1.jpg',
-			    id: 1
+			    id: 1,
+			    linkUrl:'Dosa'
 			  },
 			  {
 			    title: 'Idli Varieties',
 			    imageUrl: 'https://img.onmanorama.com/content/dam/mm/en/food/foodie/images/2019/10/1/idli.jpg.transform/onm-articleimagemob/image.jpg',
-			    id: 2
+			    id: 2,
+			    linkUrl:''
 			  },
 			  {
 			    title: 'Vada Varieties',
 			    imageUrl: 'https://st.depositphotos.com/3147737/4982/i/600/depositphotos_49827853-stock-photo-medu-vada-a-south-indian.jpg',
-			    id: 3
+			    id: 3,
+			    linkUrl:''
 			  },
 			  {
 			    title: 'Upma Varieties',
 			    imageUrl: 'https://image.shutterstock.com/image-photo/upma-made-samolina-rava-most-260nw-1904780035.jpg',
 			    size: 'large',
-			    id: 4
+			    id: 4,
+			    linkUrl:''
 			  },
 			  {
 			    title: 'Sweet Items',
 			    imageUrl: 'https://www.merisaheli.com/wp-content/uploads/2018/12/india-desserts-pudding-rava-kesari-685x366.jpg',
 			    size: 'large',
-			    id: 5
+			    id: 5,
+			    linkUrl:''
 			  }
 			]
 		} 
@@ -43,7 +48,8 @@ class Directory extends React.Component {
 			<div className='directory-menu'>
 				{
 					this.state.sections.map(section=> (
-						<MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size}/>))
+						<MenuItem key={section.id} title={section.title} 
+						imageUrl={section.imageUrl} size={section.size} linkUrl={section.linkUrl}/>))
 				}
 			</div>
 			)
